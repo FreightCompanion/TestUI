ab6c844f50a244337e17d6167c8ace122cf4e2eb
FC-FUZ <amarom245@outlook.com>
2026-01-14 12:57:09 -0600

    Update comment about App attachment in App.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// App was attached to window in App.js
+// App is attached to window in App.js
 const container = document.getElementById("root");
 const App = window.App;
 
